e02cc8964daa9de1eaed9d466edbcf6b39fd7827
Letlhogonono Chibua <bonganich@allangray.co.za>
2022-07-06 09:14:52 +0200

    adding sleep calculator to repository

---
+++ sleepCalculator.js
@@ -0,0 +1,53 @@
+
+// This progam calculates sleep debt
+
+// Function to determine how many hours of sleep you got each night of the week
+const getSleepHours = day => {
+  if (day === 'monday') {
+    return 7
+  } else if (day === 'tuesday') {
+    return 6
+  } else if (day === 'wednesday') {
+    return 8
+  } else if (day === 'thursday') {
+    return 6
+  } else if (day === 'friday') {
+    return 7
+  } else if (day === 'saturday') {
+    return 8
+  } else if (day === 'sunday') {
+    return 7
+  }
+}  
+
+// console.log(getSleepHours('thursday'));
+
+// Function to get the total sleep hours that you actually slept
+const getActualSleepHours = () => {
+  return getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
+};
+
+// Function to get the ideal sleep hours that you prefer
+const getIdealSleepHours = () => {
+  let idealHours = 8 
+  return idealHours*7
+};
+
+// console.log((getActualSleepHours()));
+// console.log(getIdealSleepHours());
+
+// Function to get the sleep debt
+const calculateSleepDebt = () => {
+  let actualSleepHours = getActualSleepHours();
+  let idealSleepHours = getIdealSleepHours();
+  if (actualSleepHours === idealSleepHours) {
+    console.log('user got the perfect amount of sleep')
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log('You got ' + (idealSleepHours - actualSleepHours) + ' hour(s) more sleep than you needed this week');
+  } else if(actualSleepHours < idealSleepHours) {
+    console.log('You got ' + (idealSleepHours - actualSleepHours) + ' hour(s) less sleep than you needed this week');
+  }
+} 
+
+// Calling the function to run the program
+calculateSleepDebt()
